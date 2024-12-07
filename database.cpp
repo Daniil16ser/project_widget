7ed36b108e62ae784b4cdd17d2ff0d3b78edeae4
Daniil16ser <122935170+Daniil16ser@users.noreply.github.com>
2024-12-07 11:24:01 +0300

    Add files via upload

---
+++ database.cpp
@@ -0,0 +1,13 @@
+#include <sqlite3.h>
+#include <iostream>
+
+int main(){
+    std::cout << "Sqlite version: " << sqlite3_libversion() << std::endl;
+    sqlite3* mydb;
+    std::cout << sqlite3_open("test.db", &mydb) << std::endl;
+
+    std::cout << "Database_file:" << mydb <<  std::endl;
+    std::cout << sqlite3_close(mydb);
+
+    return 0;
+}
